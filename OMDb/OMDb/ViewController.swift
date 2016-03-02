3427a74f07ec161d7b44a9ea96e6d9341566ddc8
Mariana Alvarez <marianaalvarezdecarvalho@hotmail.com>
2016-03-02 20:52:46 -0300

    Commit inicial.

---
+++ OMDb/OMDb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OMDb
+//
+//  Created by Mariana Alvarez on 02/03/16.
+//  Copyright © 2016 Mariana Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
